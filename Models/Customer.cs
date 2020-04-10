59cc9e053d6953c7b8a59eddbbd9d89b7d064a1a
Biswajit <biswajitpatro888outlook.com>
2020-04-10 12:59:35 +0530

    Changes to be committed:

---
+++ Models/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EMployeeApi.Models
+{[Table("Customer")]
+    public class Customer
+    {[Key]
+        public int Cid { get; set; }
+        public string Cname{ get; set; }
+        public DateTime dob{ get; set; }
+        [MaxLength(4)]
+        public int Pin { get; set; }
+    }
+}
---
+++ Models/CustomerContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EMployeeApi.Models
+{
+    public class CustomerContext : DbContext
+    {
+        public CustomerContext( DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Customer> customers{ get; set; }
+    }
+}
